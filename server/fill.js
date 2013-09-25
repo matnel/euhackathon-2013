5c1570ea678632238fcd80f8de23d7d1f93e0cfe
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-09-25 11:28:10 +0200

    Fill database

--- server/fill.js
+++ server/fill.js
@@ -1,4 +1,4 @@
-function multichoice(title, choices, correct) {
+function multichoice(db, id, title, choices, correct) {
     _q = {};
     _q.type = "multichoice";
     _q.title = title;
@@ -6,23 +6,23 @@ function multichoice(title, choices, correct) {
 
     _q.choices = [];
 
-    var answers = [];
+    var answers = {};
     for( var i = 0; i < choices.length; i++ ) {
         _q.choices.push( {
             name : choices[i]
         } );
 
-        answers.push( {
-            answer : choices[i],
-            amount : 0
-        })
-
+        answers[ choices[i] ] = 0;
     }
 
-    return JSON.stringify( _q );
+    console.log( answers );
+
+    db.set('question-' + id, JSON.stringify( _q ) );
+
+    db.set('answer-' + id, JSON.stringify( answers ) );
 }
 
-function slide(title, min, max, correct) {
+function slide(db, id, title, min, max, correct) {
     _q = {};
     _q.type = "slide";
     _q.title = title;
@@ -31,22 +31,25 @@ function slide(title, min, max, correct) {
     _q.min = min;
     _q.max = max;
 
-    return JSON.stringify( _q );
+    db.set('question-' + id, JSON.stringify( _q ) );
+
+    var answers = {};
+
+    for( var i = min; i <= max; i++ ) {
+        answers[ i ] = 0;
+    }
+    db.set('answer-' + id, JSON.stringify( answers ) );
 }
 
 exports.db_init = function( db ) {
 
-    db.set('question-1', slide("During 2013 United States made 11000-12000 information requests to Facebook. How many percent (%) of these were accepted?", 0, 100, 79) );
-    db.set('question-2', multichoice("From 2010 to 2012 the number of Germany’s information requests to Google increased. How many times more information requests were made in 2012 than in 2010?", ['Same amount', 'Twice as much', 'Ten times more'], 1) );
-    db.set('question-3', multichoice("Total of 71 countries made information request to Facebook during Jan-July 2013. Which of these countries made most of the requests per capita?", ['Spain', 'Sweden', 'France'], 2));
-    db.set('question-4', multichoice("By which factor did the overall number of information request to Google increase from 2010 to 2012?", ['1.5', '2', '5'], 0));
-    db.set('question-5', multichoice("Following companies were asked to give data to US government during 2012. In the process companies review these requests and check their legality. Which of the following companies had the lowest acceptance rate?", ['Twitter', 'Microsoft', 'Google', 'LinkedIn'],0));
-    db.set('question-6', slide("There are 206 sovereign countries in the World according to Wikipedia. How many of these made requests to Yahoo! to access their data in 2013?", 0, 206, 17));
-    db.set('question-7', multichoice("Following companies were asked to give data to US government during 2012. In the process companies review these requests and check the their legality. Which of the following companies has the highest acceptance rate?", ['Twitter', 'Microsoft', 'Google', 'LinkedIn'], 2));
+    slide(db, 1, "During 2013 United States made 11000-12000 information requests to Facebook. How many percent (%) of these were accepted?", 0, 100, 79);
+    multichoice(db, 2, "From 2010 to 2012 the number of Germany’s information requests to Google increased. How many times more information requests were made in 2012 than in 2010?", ['Same amount', 'Twice as much', 'Ten times more'], 1);
+    multichoice(db, 3, "Total of 71 countries made information request to Facebook during Jan-July 2013. Which of these countries made most of the requests per capita?", ['Spain', 'Sweden', 'France'], 2);
+    multichoice(db, 4, "By which factor did the overall number of information request to Google increase from 2010 to 2012?", ['1.5', '2', '5'], 0);
+    multichoice(db, 5, "Following companies were asked to give data to US government during 2012. In the process companies review these requests and check their legality. Which of the following companies had the lowest acceptance rate?", ['Twitter', 'Microsoft', 'Google', 'LinkedIn'],0);
+    slide(db, 6, "There are 206 sovereign countries in the World according to Wikipedia. How many of these made requests to Yahoo! to access their data in 2013?", 0, 206, 17);
+    multichoice(db, 7, "Following companies were asked to give data to US government during 2012. In the process companies review these requests and check the their legality. Which of the following companies has the highest acceptance rate?", ['Twitter', 'Microsoft', 'Google', 'LinkedIn'], 2);
 }
 
-exports.clean = function(db) {
-    for( var i = 1; i <= 7; i++ ) {
-        db.set('answer-' + i, "{}");
-    }
-}
+exports.clean = function(db) {}
